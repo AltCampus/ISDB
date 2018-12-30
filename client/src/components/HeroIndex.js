91cca4c60fccdb16a383cd6bef2032191009e75e
Abhishek Shastri <shastri48@gmail.com>
2018-12-30 18:10:03 +0530

    display company cards

--- client/src/components/HeroIndex.js
+++ client/src/components/HeroIndex.js
@@ -2,11 +2,17 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { getAllCompanyData } from '../actions/actionsCreator';
+import rootReducer from '../reducers';
+import CompanyCard from './CompanyCard';
 
+let mapStateToProps = state => {
+	return state;
+}
 class HeroIndex extends Component {
 	componentWillMount = () => {
 		this.props.dispatch(getAllCompanyData())
 	}
+
 
   render() {
     return (
@@ -16,162 +22,13 @@ class HeroIndex extends Component {
 						<input className="hero-search" type="text" name="search-box" placeholder="search"/>
 						<i className="fas fa-search"></i>
 					</div>
-
 					<section className="card-sec">
-						<div className="small-card">
-							<img id="logo" src={require("./../media/zomato.png")}/>
-							<div className="right">
-								<h2 is="title" >Zomato</h2>
-								<div id="details" className="details">
-									<span>
-									<i className="fas fa-map-marker-alt"></i>
-									Delhi
-									</span>
-									<span> Series E </span>
-								</div>
-							</div>
-						</div>
-
-					<div className="small-card">
-						<img id="logo" src={require("./../media/hackerearth.png")}/>
-						<div className="right">
-							<h2 is="title" >HackerEarth</h2>
-							<div id="details" className="details">
-								<span>
-									<i className="fas fa-map-marker-alt"></i>
-									Bangalore
-								</span>
-								<span> Series A </span>
-							</div>
-						</div>
-					</div>
-
-					<div className="small-card">
-						<img id="logo" src={require("./../media/zoomcar.png")}/>
-						<div className="right">
-							<h2 is="title" >InstaMojo</h2>
-							<div id="details" className="details">
-								<span>
-									<i className="fas fa-map-marker-alt"></i>
-									Bangalore
-								</span>
-								<span> Series B</span>
-							</div>
-						</div>
-					</div>
-
-					<div className="small-card">
-						<img id="logo" src={require("./../media/lookup.png")}/>
-						<div className="right">
-							<h2 is="title" >LookUp</h2>
-							<div id="details" className="details">
-								<span>
-									<i className="fas fa-map-marker-alt"></i>
-									Bangalore
-								</span>
-								<span> Seed</span>
-							</div>
-						</div>
-					</div>
-
-					<div className="small-card">
-						<img id="logo" src={require("./../media/zoomcar.png")}/>
-						<div className="right">
-							<h2 is="title" >ZoomCar</h2>
-							<div id="details" className="details">
-								<span>
-									<i className="fas fa-map-marker-alt"></i>
-									Hyderabad
-								</span>
-								<span> Series D</span>
-							</div>
-						</div>
-					</div>
-
-					<div className="small-card">
-						<img id="logo" src={require("./../media/zomato.png")}/>
-						<div className="right">
-							<h2 is="title" >Zomato</h2>
-							<div id="details" className="details">
-								<span>
-									<i className="fas fa-map-marker-alt"></i>
-									Delhi
-								</span>
-								<span> Series E </span>
-							</div>
-						</div>
-					</div>
-
-					<div className="small-card">
-						<img id="logo" src={require("./../media/hackerearth.png")}/>
-						<div className="right">
-							<h2 is="title" >HackerEarth</h2>
-							<div id="details" className="details">
-								<span>
-									<i className="fas fa-map-marker-alt"></i>
-									Bangalore
-								</span>
-								<span> Series A </span>
-							</div>
-						</div>
-					</div>
-
-					<div className="small-card">
-						<img id="logo" src={require("./../media/zomato.png")}/>
-						<div className="right">
-							<h2 is="title" >InstaMojo</h2>
-							<div id="details" className="details">
-								<span>
-									<i className="fas fa-map-marker-alt"></i>
-									Bangalore
-								</span>
-								<span> Series B</span>
-							</div>
-						</div>
-					</div>
-
-					<div className="small-card">
-						<img id="logo" src={require("./../media/lookup.png")}/>
-						<div className="right">
-							<h2 is="title" >LookUp</h2>
-							<div id="details" className="details">
-								<span>
-									<i className="fas fa-map-marker-alt"></i>
-									Bangalore
-								</span>
-								<span> Seed</span>
-							</div>
-						</div>
-					</div>
-
-					<div className="small-card">
-						<img id="logo" src={require("./../media/zoomcar.png")}/>
-						<div className="right">
-							<h2 is="title" >ZoomCar</h2>
-							<div id="details" className="details">
-								<span>
-									<i className="fas fa-map-marker-alt"></i>
-									Hyderabad
-								</span>
-								<span> Series D</span>
-							</div>
-						</div>
-					</div>
-
-					<div className="small-card">
-						<img id="logo" src={require("./../media/zoomcar.png")}/>
-						<div className="right">
-							<h2 is="title" >ZoomCar</h2>
-							<div id="details" className="details">
-								<span>
-									<i className="fas fa-map-marker-alt"></i>
-									Hyderabad
-								</span>
-								<span> Series D</span>
-							</div>
-						</div>
-					</div>
-
+					
+					{
+						this.props.companies.map((value, index) => {
+							return <CompanyCard value = {value} key = {index} />
+						})
+					}
 
 						<div className="small-card">
 							<h2 className="title placeholder" >More...</h2>
@@ -183,6 +40,8 @@ class HeroIndex extends Component {
   }
 }
 
-export default connect()(HeroIndex);
+export default connect(
+	mapStateToProps
+)(HeroIndex);
 
 
