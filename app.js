4f31d413cd380ccc4840a6d03be8edc223482f42
praveen-me <praveenkrsaini0@gmail.com>
2018-12-30 20:19:57 +0530

    Added passport module

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpackConfig = require('./webpack.config');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
 // const MongoStore = require('connect-mongo')(mongoose);
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -15,9 +17,9 @@ const app = express();
 app.use(cors());
 
 // connecting mongodb
-mongoose.connect('mongodb://localhost/ISDB', { useNewUrlParser: true }, function(err, connection) {
-  if(err) throw err;
-  else console.log('connected to mongodb');
+mongoose.connect('mongodb://localhost/ISDB', { useNewUrlParser: true }, (err, connection) => {
+  if (err) throw err;
+  console.log('connected to mongodb');
 });
 
 // use middleware for parsing body into json
@@ -28,17 +30,26 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, '/server/views'));
 
-
 // Middleware of webpack
 if (process.env.NODE_ENV === 'development') {
   console.log('in webpack hot middleware');
   const compiler = webpack(webpackConfig);
-  
   app.use(webpackDevMiddleware(compiler, {
     noInfo: true,
     publicPath: webpackConfig.output.publicPath,
   }));
 }
+
+// Setting middleware for passport and session
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+require('./server/modules/passport')(passport);
 
 // routes for API Endpoints
 app.use('/api/v1', require('./server/routes/api.v1'));
